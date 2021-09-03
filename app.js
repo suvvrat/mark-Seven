77ae903ef97f45275e0984ef0605e38ee3c0dffe
Suvrat Verma <75042545+suvvrat@users.noreply.github.com>
2021-09-03 20:10:20 +0530

    configured app.js file

---
+++ app.js
@@ -0,0 +1,28 @@
+
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput= document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+var serverURL = "https://api.funtranslations.com/translate/doge.json";
+
+btnTranslate.addEventListener("click", clickHandler);
+
+function clickHandler(){
+    var inputText = txtInput.value;
+    fetch(getTranslationURL(inputText))
+        .then(response=>response.json())
+        .then(json=>{
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;
+        })
+    .catch(errorHandler);    
+ }
+
+function getTranslationURL(input){
+    return serverURL + "?" + "text=" + input;
+}
+
+function errorHandler(error){
+    console.log("An error occured!",error);
+    alert("Something wrong with the server: Try again after sometime :( ");
+}
+
